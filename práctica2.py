a1d8b2347919fb0608c4fecd7b2b5b429939788a
Francis Grullon <francisclaritssa@gmail.com>
2020-09-18 00:19:04 -0400

    práctica 2 incompleta

---
+++ práctica2.py
@@ -0,0 +1,53 @@
+# 1
+
+# numero= 1
+# cantidad = 0
+# suma= 0
+# while numero != 0:
+#     numero= int(input("ingrese un numero: "))
+#     if numero != 0:
+#         suma= suma + numero
+#         cantidad= cantidad + 1
+
+#     print(f"usted ingreso {cantidad} y la suma es:{suma} ")
+
+
+    #2
+# def menu ():
+#     print ("bienvenido al mneú: 1. convertir de celcius a Fahrenheit, 2. converit de dolar a peso, 3. convertir de metros a pies, 4. salir")
+
+
+# menu ()
+# n= int (input ("ingrese el numero de la opcion deseada:"))
+# while n < 4:
+#     if n == 1 :
+#         x= input ("ingrese el grado en celcius_: ")
+#         print ((int (x) * 9/5) + 32)
+#         menu ()
+#         n= int (input ("ingrese el numero de la opcion deseada:"))
+#     elif n== 2: 
+#          y= input ("ingrese la cantidad en dolar: ")
+#          print ((int (y) * 58.44))
+#          menu ()
+#          n= int (input ("ingrese el numero de la opcion deseada:"))
+#     else:
+#         n== 3
+#         z= int(input("ingrese la cantidad en metros: "))
+#         print ((int (z) * 3.281))
+#         menu ()
+#         n= int (input ("ingrese el numero de la opcion deseada:"))
+
+# print ("Gracias por utilizar mi programa")
+#3
+# n = int (input("ingrese el numero deseado: "))
+# e= 5
+# while  e <= 1000 :
+#     print (n * e)
+#     e += 5 
+
+
+
+    
+             
+            
+
